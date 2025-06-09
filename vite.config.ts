503b57339a72ab2a45617900faa4fd0cc92095c8
sanket140 <intern3.topai24@gmail.com>
2025-06-09 17:04:11 +0530

    Setup deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({base:'/Dating-app-Launch-page/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
